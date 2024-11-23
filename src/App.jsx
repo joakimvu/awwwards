62d6e2e16bc58f88cdb62ceeeec4b53d5e6eb590
Joakim Vu <joakimvu@icloud.com>
2024-11-23 10:40:13 +0100

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-5xl text-violet-300 font-bold">
+        Welcome to Awwwards
+      </h1>
+    </div>
+  );
+};
+
+export default App;
